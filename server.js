48cd2f01805c44c5fca7c1aee386cf81449ced18
unknown <saad123mn123@gmail.com>
2024-01-06 22:06:51 +0200

    update email

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const { BetaAnalyticsDataClient } = require('@google-analytics/data');
 
 const app = express();
 
@@ -24,7 +25,7 @@ app.post('/submitForm', (req, res) => {
 
   const mailOptions = {
     from: email, // Sender email address
-    to: 'Khaledsalim@yahoo.com', // Recipient email address
+    to: 'info@elsewedy-automation.com', // Recipient email address
     subject: 'Website Messages',
     text: `
       First Name: ${firstName}\n
@@ -44,7 +45,42 @@ app.post('/submitForm', (req, res) => {
     }
   });
 });
+const analyticsDataClient = new BetaAnalyticsDataClient();
+const propertyId = '313683933';
 
+app.get('/api/analytics', async (req, res) => {
+  
+  try {
+    const [response] = await analyticsDataClient.runReport({
+      property: `properties/${propertyId}`,
+      dateRanges: [
+        {
+          startDate: '2020-03-31',
+          endDate: 'today',
+        },
+      ],
+      dimensions: [
+        {
+          name: 'city',
+        },
+      ],
+      metrics: [
+        {
+          name: 'activeUsers',
+        },
+      ],
+    });
+
+    const reportData = response.rows.map(row => ({
+      city: row.dimensionValues[0],
+      activeUsers: row.metricValues[0],
+    }));
+    console.log(reportData);
+    res.status(200).json({ reportData });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
